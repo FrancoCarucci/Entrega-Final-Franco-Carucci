35d8dbb7a7f8a99bbc49f8c4d022e80d7ddc16bc
FrancoCarucci <francocaru1@gmail.com>
2024-11-01 11:14:50 -0300

    usuarios_roles.sql

--- usuarios_roles.sql
+++ usuarios_roles.sql
@@ -23,3 +23,18 @@ GRANT ALL PRIVILEGES ON razas TO role_crud_mascotas;
 
 -- Asignar privilegios CRUD al role_crud_adopciones
 GRANT ALL PRIVILEGES ON adopciones TO role_crud_adopciones;
+
+-- Crear usuarios con permisos de solo lectura en las vistas
+CREATE USER 'usuario_vistas'@'%' IDENTIFIED BY 'password_vistas';
+GRANT role_select_vistas TO 'usuario_vistas'@'%';
+
+-- Crear usuarios con permisos CRUD en la tabla de mascotas
+CREATE USER 'usuario_mascotas'@'%' IDENTIFIED BY 'password_mascotas';
+GRANT role_crud_mascotas TO 'usuario_mascotas'@'%';
+
+-- Crear usuarios con permisos CRUD en la tabla de adopciones
+CREATE USER 'usuario_adopciones'@'%' IDENTIFIED BY 'password_adopciones';
+GRANT role_crud_adopciones TO 'usuario_adopciones'@'%';
+
+FLUSH PRIVILEGES;
+
